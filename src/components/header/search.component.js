b3dd7fd748900e4896c060ba8d1902d64ce3f6dc
Tuấn Đào <daotuan1903@gmail.com>
2021-12-16 17:36:30 +0700

    Fixing searchResults episode 3

--- src/components/header/search.component.js
+++ src/components/header/search.component.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import { Form, ListGroup, ListGroupItem, Modal } from "react-bootstrap";
 import { autocompleteApi, placeDetailApi } from "../../api/goong.api";
-import { useNavigate, createSearchParams, generatePath } from "react-router-dom";
+import { useNavigate, createSearchParams } from "react-router-dom";
 import { SearchContext } from "../../context/searchContext";
 import "react-datepicker/dist/react-datepicker.css";
 import "./search.css";
@@ -383,7 +383,6 @@ export const OnlySearchBar = () => {
         pathname: "/search",
         search: `?${createSearchParams({ ...body })}`,
       });
-      window.location.reload();
     }
   };
 
