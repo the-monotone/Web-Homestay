01b464cffc6cf5550318d26b6ed18ab897cacb41
Tuấn Đào <daotuan1903@gmail.com>
2021-12-21 11:24:06 +0700

    Merging episode 2

--- src/components/room/roomList.jsx
+++ src/components/room/roomList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-import { RoomCard } from './roomCard'
+import { RoomCard } from './RoomCard'
 import { useNavigate } from 'react-router-dom';
 import { LoadingCardList } from '../shared/loadingCard';
 
