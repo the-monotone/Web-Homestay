cdf8a41cf081d1390236ed6b6e735e70aa4dc937
Tuấn Đào <daotuan1903@gmail.com>
2021-12-23 14:45:25 +0700

    Change key 1

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_MAP_KEY = 'AIzaSyCsr1do2340QjymLmG1dWNqX1gw5P9gqLI';
+export const GOOGLE_MAP_KEY = 'AIzaSyDphxi7iZtr0hNWTQKa88Eoc9C5jEXR5IM';
 
 export const WEB_API = 'https://wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
 export const SOCKET_API = '//wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
