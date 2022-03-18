51684c4b40c344a802864b7fe0ac50660af37fdd
Pi-Cal <nnthach1024@gmail.com>
2022-03-18 15:30:17 +0700

    change key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_MAP_KEY = 'AIzaSyDDrxLAvnfAnbfmj8qwP4eBfO3KPBJIask';
+export const GOOGLE_MAP_KEY = 'AIzaSyDx5w6tgCXFdHy5dA-9qfT79v1G64A0ULo';
 
 export const WEB_API = 'https://wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
 export const SOCKET_API = '//wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
