45096b67feb86a82689956a3f710b6f391698af5
Pi-Cal <62058737+Pi-Cal@users.noreply.github.com>
2022-06-01 22:41:37 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const GOOGLE_MAP_KEY = 'AIzaSyDCwBVrDFv3Jo5i6ei-w7HM7ul9seMxigA';
 
-export const WEB_API = 'http://wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
+export const WEB_API = 'https://wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
 export const SOCKET_API = '//wehome-env.eba-ujwahtza.ap-southeast-1.elasticbeanstalk.com';
